a206dec0d9b4ff0b4038b7bf3c0528664e22bc5d
Juri <juri.strumpflohner@gmail.com>
2024-01-29 13:09:51 +0100

    ..

---
+++ apps/flight-simulator-e2e/src/e2e/app3.cy.ts
@@ -0,0 +1,15 @@
+import { getGreeting } from '../support/app.po';
+
+describe('flight-simulator-e2e three', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    cy.wait(2000);
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains(/Welcome/);
+  });
+});
